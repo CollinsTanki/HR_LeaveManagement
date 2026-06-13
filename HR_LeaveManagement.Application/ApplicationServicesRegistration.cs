e10c47e416413b4cf52b2f0bfef1024827b60224
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2026-06-13 10:23:25 +0300

    Added MediatR libraries used in ASP.NET Core Clean Architecture to implement the Mediator Pattern and CQRS (Command Query Responsibility Segregation).

--- HR_LeaveManagement.Application/ApplicationServicesRegistration.cs
+++ HR_LeaveManagement.Application/ApplicationServicesRegistration.cs
@@ -4,21 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 
 namespace HR_LeaveManagement.Application
 {
     public static class ApplicationServicesRegistration
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services)
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
 
             // Register application services here, e.g.:
             // services.AddTransient<IMyService, MyService>();
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(assemblies: new[] { Assembly.GetExecutingAssembly() });
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
         }
     }
 }
