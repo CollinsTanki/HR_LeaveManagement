3fd3a1b72cd477ae113e9932685b53bd93e6da0d
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2026-06-15 13:23:20 +0300

    Added a Query Handler in the Application Layer of a Clean Architecture project that uses CQRS + MediatR.
    Its responsibility is:
    
    Receive a request to get all leave types, execute the business logic, retrieve data from the repository, map the entities to DTOs, and return the result.

---
+++ HR_LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using HR_LeaveManagement.Application.DTOs;
+using HR_LeaveManagement.Application.Features.LeaveTypes.Requests;
+using HR_LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository)
+        {
+           this._leaveTypeRepository = leaveTypeRepository;
+        }
+        public Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            // TODO: implement using your repository / mapper
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HR_LeaveManagement.Application/Features/LeaveTypes/Requests/GetLeaveTypeListRequest.cs
@@ -0,0 +1,14 @@
+﻿using HR_LeaveManagement.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDto>>
+    {
+    }
+}
